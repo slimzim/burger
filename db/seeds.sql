329e3f857600ab790a3f0f233a3da426a1cfd852
slimzim <slimzim@gmail.com>
2020-07-15 16:25:08 -0500

    added start script to package.json

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,5 +1,18 @@
-INSERT INTO burgers (burger_name) VALUES ('Hamburger');
-INSERT INTO burgers (burger_name) VALUES ('Cheeseburger');
-INSERT INTO burgers (burger_name) VALUES ('Double Cheeseburger');
+SET SQL_SAFE_UPDATES=0;
 
-SELECT * FROM burgers
+CREATE DATABASE burgers_db;
+USE burgers_db;
+DROP TABLE burgers;
+CREATE TABLE burgers
+(
+	id int NOT NULL AUTO_INCREMENT,
+	burger_name varchar(255) NOT NULL,
+	devoured BOOLEAN DEFAULT false,
+	PRIMARY KEY (id)
+);
+
+SELECT * FROM burgers;
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Hamburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Cheeseburger', false);
